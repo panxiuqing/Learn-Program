56beb4a949e2408ef4e97486ddd3682ef3979492
panxiquing <panxiuqing@gmail.com>
2012-09-07 13:26:47 +0800

    update doubanfm

--- python/doubanfm.py
+++ python/doubanfm.py
@@ -1,7 +1,7 @@
 #coding=utf-8
 #!/usr/bin/env python
 
-import os
+import os, subprocess
 import urllib2, json
 
 
@@ -30,12 +30,13 @@ get_list = [('app_name=', app_name),
 get_string = "&".join(key + value for key, value in get_list)
 final_url = fm_url + get_string
 
-mp3_info = {'title': '曲目', 'picture': '专辑封面', 'albumtitle': '专辑', 'company': '公司', 'artist': '演唱者', 'like': '喜欢'}
 
 while True:
     resp = urllib2.urlopen(final_url)
     play_list = json.loads(resp.read())['song']
     for mp3 in play_list:
-        for key in mp3_info:
-            print mp3_info[key], ':', (12 - 2*len(mp3_info[key]))*' ' , mp3[key]
-        os.system('mplayer %s > /dev/null' % mp3['url'])
+        mp3_img = mp3['picture']
+        mp3_title = mp3['title'].encode('utf-8')
+        mp3_info = (mp3['albumtitle'] + '\n' + mp3['artist'])
+        subprocess.call(['notify-send', '-i', mp3_img, mp3_title, mp3_info])
+        subprocess.call(['mplayer', mp3['url']])
