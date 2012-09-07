f9fc88c7f2987952721e86bea5e588800ea459bb
panxiquing <panxiuqing@gmail.com>
2012-09-07 14:52:20 +0800

    update doubanfm

--- python/doubanfm.py
+++ python/doubanfm.py
@@ -1,8 +1,9 @@
 #coding=utf-8
 #!/usr/bin/env python
 
-import os, subprocess
-import urllib2, json
+import subprocess
+import urllib2, urllib, json
+import tempfile
 
 
 #api_string = 'http://www.douban.com/j/app/radio/people?app_name=radio_desktop_win&version=100&user_id=&expire=&token=&sid=&h=&channel=1&type=n'
@@ -30,13 +31,15 @@ get_list = [('app_name=', app_name),
 get_string = "&".join(key + value for key, value in get_list)
 final_url = fm_url + get_string
 
+img_file = tempfile.NamedTemporaryFile()
 
 while True:
     resp = urllib2.urlopen(final_url)
     play_list = json.loads(resp.read())['song']
     for mp3 in play_list:
-        mp3_img = mp3['picture']
+        mp3_img_url = mp3['picture']
+        urllib.urlretrieve(mp3_img_url, img_file.name)
         mp3_title = mp3['title'].encode('utf-8')
         mp3_info = (mp3['albumtitle'] + '\n' + mp3['artist'])
-        subprocess.call(['notify-send', '-i', mp3_img, mp3_title, mp3_info])
+        subprocess.call(['notify-send', '-i', img_file.name, mp3_title, mp3_info])
         subprocess.call(['mplayer', mp3['url']])
